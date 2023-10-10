27bf6c2f7c351973d0cfba9a0144608d55adbb40
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-10-10 11:40:17 -0500

    add routing users

---
+++ routes/crudUsersRouter.js
@@ -0,0 +1,22 @@
+const {Router} = require('express');
+const UserModel = require('../models/user');
+
+const router = Router();
+
+router.post('/users', (req, res) => {
+
+});
+
+router.get('/users', (req, res) => {
+
+});
+
+router.put('/users/:id', (req, res) => {
+
+});
+
+router.delete('/users/:id', (req, res) => {
+
+});
+
+module.exports = router;
